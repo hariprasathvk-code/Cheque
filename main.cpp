da1613946c5e343176753cce9ae6a7c0bf357a57
Hariprasath Krishnamoorthi <hariprasath.krishnamoorthi@psiog.com>
2025-08-17 02:52:34 -0700

    main function commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Global variables 
+string senderAcc, receiverAcc;
+string bankCard;
+double value;
+int days;
+char authorize;
+
+// Initialization Function
+void initialization() {
+    
+}
+
+// Process Function
+void process() {
+    
+}
+// Termination Function
+void termination() {
+    
+}
+// Main Function
+int main() {
+    initialization();
+    process();
+    termination();
+    return 0;
+}
